6529499f33d5e4322ea9f561fc2517afff09220a
sixro <me@sixro.net>
2020-03-19 21:48:21 +0100

    Added Interval object

--- src/main/java/themissedobjects/time/Interval.java
+++ src/main/java/themissedobjects/time/Interval.java
@@ -1,4 +1,96 @@
 package themissedobjects.time;
 
-public class Interval {
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalUnit;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Represents an interval of {@code Temporal}s.
+ *
+ * <p>
+ * Because it is based on {@code Temporal}, you can use {@code LocalDate}, {@code ZonedDateTime}, {@code Instant} or even {@Year}...
+ * </p>
+ * <p>
+ * The textual representation uses a ISO-8601 format that in date 2019 is in draft state yet, but it is clear:
+ * </p>
+ * <ul>
+ *     <li>an interval between 2 times has a {@code /} between. E.g. {@code 2000/2010}</li>
+ *     <li>an open interval has the missing part represented by {@code ..}. E.g. {@code ../2010} or {@code 2020/..}</li>
+ * </ul>
+ *
+ * @param <T> a Temporal
+ *
+ * @author Roberto Simoni
+ * @since 1.0
+ */
+public class Interval<T extends Temporal> {
+
+    private static final Map<Class<? extends Temporal>, ChronoUnit> TEMPORAL_UNIT_BY_TEMPORAL = newTemporalUnitByTemporal();
+
+    private final T from;
+    private final T to;
+
+    private Interval(T from, T to) {
+        if (from != null && to != null) {
+            TemporalUnit temporalUnit = TEMPORAL_UNIT_BY_TEMPORAL.get(to.getClass());
+            if (from.until(to, temporalUnit) <= 0)
+                throw new IllegalArgumentException("'to' must be greater than 'from' (from is '" + from + "', to is '" + to + "')");
+        } else if (from == null && to == null)
+            throw new IllegalArgumentException("At least one between 'from' and 'to' is required (from is '" + from + "', to is '" + to + "')");
+
+        this.from = from;
+        this.to = to;
+    }
+
+    public static <T extends Temporal> Interval<T> between(T from, T to) {
+        return new Interval<T>(from, to);
+    }
+
+    public static <T extends Temporal> Interval<T> beginningFrom(T from) {
+        return new Interval<T>(from, null);
+    }
+
+    public static <T extends Temporal> Interval<T> endingTo(T to) {
+        return new Interval<T>(null, to);
+    }
+
+    /**
+     * Returns {@code true} if the specified temporal is contained in this interval.
+     * @param t a temporal
+     * @return {@code true} if the specified temporal is contained in this interval, otherwise {@code false}
+     */
+    public boolean contains(T t) {
+        TemporalUnit temporalUnit = TEMPORAL_UNIT_BY_TEMPORAL.get(t.getClass());
+        if (to != null && from != null) {
+            long fromTo = from.until(to, temporalUnit);
+            long tTo = t.until(to, temporalUnit);
+            return fromTo >= tTo && tTo > 0;
+        } else if (to != null) {
+            long tTo = t.until(to, temporalUnit);
+            return tTo > 0;
+        } else {
+            long fromT = from.until(t, temporalUnit);
+            return fromT >= 0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return Objects.toString(from, "..") + "/" + Objects.toString(to, "..");
+    }
+
+    private static Map<Class<? extends Temporal>, ChronoUnit> newTemporalUnitByTemporal() {
+        Map<Class<? extends Temporal>, ChronoUnit> map = new HashMap<>();
+        map.put(Year.class, ChronoUnit.YEARS);
+        map.put(YearMonth.class, ChronoUnit.MONTHS);
+        map.put(LocalDate.class, ChronoUnit.DAYS);
+        map.put(ZonedDateTime.class, ChronoUnit.MILLIS);
+        map.put(Instant.class, ChronoUnit.MILLIS);
+        return map;
+    }
+
 }
---
+++ src/test/java/themissedobjects/time/IntervalTest.java
@@ -0,0 +1,92 @@
+package themissedobjects.time;
+
+import org.junit.Test;
+
+import java.time.*;
+
+import static org.junit.Assert.*;
+
+public class IntervalTest {
+
+    @Test public void contains_with_LocalDate() {
+        Interval<LocalDate> i = Interval.between(LocalDate.parse("2020-01-01"), LocalDate.parse("2020-03-01"));
+        assertTrue(i.contains(LocalDate.parse("2020-02-01")));
+        assertFalse(i.contains(LocalDate.parse("2020-04-01")));
+        assertFalse(i.contains(LocalDate.parse("2019-12-01")));
+    }
+
+    @Test public void contains_with_LocalDate_on_edges() {
+        Interval<LocalDate> i = Interval.between(LocalDate.parse("2020-01-01"), LocalDate.parse("2020-03-01"));
+        assertFalse(i.contains(LocalDate.parse("2019-12-31")));
+        assertTrue(i.contains(LocalDate.parse("2020-01-01")));
+        assertTrue(i.contains(LocalDate.parse("2020-02-29")));
+        assertFalse(i.contains(LocalDate.parse("2020-03-01")));
+    }
+
+    @Test public void contains_with_ZonedDateTime() {
+        Interval<ZonedDateTime> i = Interval.between(ZonedDateTime.parse("2020-01-01T12:00:00Z"), ZonedDateTime.parse("2020-01-01T14:00:00Z"));
+        assertTrue(i.contains(ZonedDateTime.parse("2020-01-01T13:00:00Z")));
+        assertFalse(i.contains(ZonedDateTime.parse("2020-01-01T15:00:00Z")));
+        assertFalse(i.contains(ZonedDateTime.parse("2020-01-01T11:00:00Z")));
+    }
+
+    @Test public void contains_with_ZonedDateTime_on_edges() {
+        Interval<ZonedDateTime> i = Interval.between(ZonedDateTime.parse("2020-01-01T12:00:00Z"), ZonedDateTime.parse("2020-01-01T14:00:00Z"));
+        assertFalse(i.contains(ZonedDateTime.parse("2020-01-01T11:59:59.000Z")));
+        assertTrue(i.contains(ZonedDateTime.parse("2020-01-01T12:00:00.000Z")));
+        assertTrue(i.contains(ZonedDateTime.parse("2020-01-01T13:59:59.000Z")));
+        assertFalse(i.contains(ZonedDateTime.parse("2020-01-01T14:00:00Z")));
+    }
+
+    @Test public void contains_with_Year() {
+        Interval<Year> i = Interval.between(Year.of(2000), Year.of(2020));
+        assertTrue(i.contains(Year.of(2010)));
+        assertFalse(i.contains(Year.of(1999)));
+        assertFalse(i.contains(Year.of(2030)));
+    }
+
+    @Test public void contains_with_YearMonth() {
+        Interval<YearMonth> i = Interval.between(YearMonth.parse("2000-01"), YearMonth.parse("2000-03"));
+        assertTrue(i.contains(YearMonth.parse("2000-02")));
+        assertFalse(i.contains(YearMonth.parse("1999-12")));
+        assertFalse(i.contains(YearMonth.parse("2001-01")));
+    }
+
+    @Test public void contains_with_Instant() {
+        Interval<Instant> i = Interval.between(Instant.parse("2020-01-01T12:00:00Z"), Instant.parse("2020-01-01T14:00:00Z"));
+        assertTrue(i.contains(Instant.parse("2020-01-01T13:00:00Z")));
+        assertFalse(i.contains(Instant.parse("2020-01-01T15:00:00Z")));
+        assertFalse(i.contains(Instant.parse("2020-01-01T11:00:00Z")));
+    }
+
+    @Test public void with_just_the_begin() {
+        Interval<ZonedDateTime> i = Interval.beginningFrom(ZonedDateTime.parse("2020-01-01T12:00:00Z"));
+        assertFalse(i.contains(ZonedDateTime.parse("2020-01-01T11:59:59.000Z")));
+        assertTrue(i.contains(ZonedDateTime.parse("2020-01-01T12:00:00.000Z")));
+        assertTrue(i.contains(ZonedDateTime.parse("2999-01-01T00:00:00.000Z")));
+    }
+
+    @Test public void with_just_the_end() {
+        Interval<ZonedDateTime> i = Interval.endingTo(ZonedDateTime.parse("2020-01-01T12:00:00Z"));
+        assertFalse(i.contains(ZonedDateTime.parse("2020-01-01T12:00:00.000Z")));
+        assertTrue(i.contains(ZonedDateTime.parse("2020-01-01T11:59:59.999Z")));
+        assertTrue(i.contains(ZonedDateTime.parse("0999-01-01T00:00:00.000Z")));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void validate() {
+        Interval.between(Year.of(2000), Year.of(1000));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void validate_on_same_values() {
+        Interval.between(Year.of(2000), Year.of(2000));
+    }
+
+    @Test public void textual_representation() {
+        assertEquals("2020-01-01/2020-03-01", Interval.between(LocalDate.parse("2020-01-01"), LocalDate.parse("2020-03-01")).toString());
+        assertEquals("2020-01-01/..", Interval.beginningFrom(LocalDate.parse("2020-01-01")).toString());
+        assertEquals("../2020-03-01", Interval.endingTo(LocalDate.parse("2020-03-01")).toString());
+        assertEquals("../2020", Interval.endingTo(Year.of(2020)).toString());
+    }
+}
