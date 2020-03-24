a690d0b837719f407232a88b69715aff63ddc471
sixro <me@sixro.net>
2020-03-24 01:33:44 +0100

    changed visibility of an internal constant

--- src/main/java/themissingobjects/finance/SmartDecimalFormat.java
+++ src/main/java/themissingobjects/finance/SmartDecimalFormat.java
@@ -43,7 +43,7 @@ import java.util.regex.Pattern;
  */
 public class SmartDecimalFormat extends DecimalFormat {
 
-    public static final Pattern CURRENCY_PATTERN = Pattern.compile("([^0-9\\-\\.,]*)([0-9\\-\\.,]+)([^0-9\\-\\.,]*)");
+    private static final Pattern CURRENCY_PATTERN = Pattern.compile("([^0-9\\-\\.,]*)([0-9\\-\\.,]+)([^0-9\\-\\.,]*)");
 
     private final Locale locale;
 
